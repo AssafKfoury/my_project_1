9fc914c6f5a400a1c6791b4c2718b68f29de64bd
Assaf Kfoury <kfoury@bu.edu>
2023-06-20 18:21:43 -0400

    aaa

--- MyProject1/test.lean
+++ MyProject1/test.lean
@@ -14,14 +14,12 @@ theorem easy (P Q R : Prop) (h1 : P → Q)
     h2 (h1 h3)  
 
 /- a little different from the syntax of 
-   `Blank_by_Assaf.lean` -/
+   `Blank_by_Assaf.lean` in HTPI+Lean -/
 theorem easy1 (P Q R : Prop) (h1 : P → Q)
-   (h2 : Q → R) (h3 : P) : R := by
-   have h4 : Q := (h1 h3)
-   have h5 : R := (h2 h4)
-   exact h5  
-   -- show R := (h2 h4)
-   done
+   (h2 : Q → R) (h3 : P) : R := -- by
+    have h4 : Q := h1 h3
+    show R from h2 h4  
+    -- done
 
 theorem easy2 (P Q R : Prop) (h1 : P → Q)
    (h2 : Q → R) (h3 : P) : R := sorry 
@@ -31,4 +29,28 @@ theorem easy3 (P Q R : Prop) (h1 : P → Q)
    apply h2 _ ; apply h1 _ ; exact h3 
    done
 
-#check easy2    
+lemma lem1 (P Q R : Prop) : P ∧ Q → P ∧ (Q ∨ R) :=
+   (fun hpq : P ∧ Q =>
+    have hp : P := hpq.left
+    have hq : Q := hpq.right
+    show P ∧ (Q ∨ R) from ⟨hp, Or.inl hq⟩)
+
+theorem test (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  exact hp
+  apply And.intro
+  exact hq
+  exact hp
+  done 
+ 
+
+#check easy1
+#check lem1     
+
+#check And     -- Prop → Prop → Prop
+#check Or      -- Prop → Prop → Prop
+#check Not     -- Prop → Prop
+#check Implies -- Prop → Prop → Prop
+
+#check modus_ponens 
+
