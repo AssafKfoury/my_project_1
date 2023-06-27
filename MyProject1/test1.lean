fa26d0812dff50fa5e9f74bc476e9e8cce596bca
AssafKfoury <kfoury@bu.edu>
2023-06-27 16:23:48 -0400

    aaa

---
+++ MyProject1/test1.lean
@@ -0,0 +1,31 @@
+/-# Created on 2023-06-27 -/
+
+/- Copied from Theorem Proving in Lean 4, Chapt 5, beginning
+   of section `Basic Tactics` -/
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  . intro h
+    apply Or.elim (And.right h)
+    . intro hq
+      apply Or.inl
+      apply And.intro
+      . exact And.left h
+      . exact hq
+    . intro hr
+      apply Or.inr
+      apply And.intro
+      . exact And.left h
+      . exact hr
+  . intro h
+    apply Or.elim h
+    . intro hpq
+      apply And.intro
+      . exact And.left hpq
+      . apply Or.inl
+        exact And.right hpq
+    . intro hpr
+      apply And.intro
+      . exact And.left hpr
+      . apply Or.inr
+        exact And.right hpr
+  
